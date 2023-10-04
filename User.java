bba253c7c747354c6c5259b3aac03691c7a3b184
KevinDavoust <kevin.davoust@orange.fr>
2023-10-04 19:08:57 +0200

    quest

---
+++ User.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+import java.util.List;
+
+public class User {
+
+    private Date birthday;
+    private Number size;
+
+    private Address address;
+
+    private List<Photo> photos;
+}
